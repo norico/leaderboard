1ab7ee97be609ade07f9a1d512ff0c6cfe33f13c
norico <norico.legrand@gmail.com>
2021-02-12 11:57:56 +0100

    Update app.js
    -

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ class BlindTestPlayer extends HTMLElement {
         return ["start", "increment", "current" ]
     }
 
-    constructor (value=1) {
+    constructor (value) {
         super()
         this.playerid = value
         this.innerHTML = '<div id="'+ this.playerid +'" class="input-group my-1">'+
+            '<button type="button" id="add" class="btn btn-success">+</button>' +
             '<input type="text" class="form-control" id="playername">'+
             '<span class="input-group-text" id="score">0</span>'+
         '</div>'
@@ -26,14 +27,14 @@ customElements.define('blindtest-player', BlindTestPlayer)
 document.querySelector("#addPlayer").addEventListener('click', function(){
     const container = document.getElementById("main")
     const counterElement  = document.getElementById("addPlayer")
-    let counter = counterElement.getAttribute('value')
+    let counter = parseInt(counterElement.getAttribute('value'))
     container.appendChild( new BlindTestPlayer(counter) )
-    counter = counter++
-    counterElement.setAttribute("value", counter.toString() )
+    newCounter = counter+1
+    counterElement.setAttribute("value", newCounter )
 
+    
 
 })
-
 
 
 
