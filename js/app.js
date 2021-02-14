078661569479a8cfc30778608ce06661d3e58102
norico <norico.legrand@gmail.com>
2021-02-14 16:04:12 +0100

    release 1.0

--- js/app.js
+++ js/app.js
@@ -1,33 +1,5 @@
-class BlindTestPlayer extends HTMLElement {
-    /*
-    static get observedAttributes() {
-        return []
-    }
-    */
-
-    constructor (value) {
-        super()
-        this.playerid = value
-        this.innerHTML =  '<div id="'+ this.playerid +'" class="input-group my-1">'+
-            '<button type="button" onclick="remove('+ this.playerid +')" class="btn btn-danger">x</button>' +
-            '<button type="button" onclick="scoring('+ this.playerid +', \'remove\' )" class="btn btn-warning">-</button>' +
-            '<button type="button" onclick="scoring('+ this.playerid +', \'add\' )" class="btn btn-success">+</button>' +
-            '<input type="text" class="form-control" id="playername">' +
-            '<span class="input-group-text" id="score">0</span>'+
-            '</div>'
-    }
-    attributeChangedCallback (name, oldValue, newValue) {
-        if (name === 'current' && oldValue !== newValue) {
-            console.log( name )
-        }
-
-    }
-}
-customElements.define('blindtest-player', BlindTestPlayer)
-
-/*  ------  */
-
-const maxpoints = 20;
+const maxpoints = 3;
+const pointText = (maxpoints > 1) ? ' points' : ' point'
 
 document.querySelector("#reset").addEventListener('click', function(){
     document.querySelectorAll("span#score").forEach(e => { e.innerHTML = "0" })
@@ -42,11 +14,17 @@ document.querySelector("#addPlayer").addEventListener('click', function(){
     counterElement.setAttribute("value", newCounter )
 })
 
+const info     = document.createElement("small")
+const infoText = document.createTextNode(maxpoints + pointText)
+
+info.classList.add("text-muted", "align-self-center", "pt-4", 'me-2' )
+
+info.appendChild(infoText)
+document.getElementById("title").appendChild(info)
 
 function remove(id){
     document.getElementById(id).parentNode.remove()
 }
-
 
 function scoring(id, action){
     let current_player = id
@@ -54,8 +32,7 @@ function scoring(id, action){
     let playername = document.getElementById(id).children.namedItem('playername').value
     let current_score = parseInt(score.innerHTML)
     let new_score = current_score
-
-    if ( action === 'add' && current_score < maxpoints ){
+    if ( action === 'add' && current_score < maxpoints && playername != "" ){
         new_score = current_score+1
         console.log("Add point to " + playername )
     }
@@ -64,11 +41,34 @@ function scoring(id, action){
         console.log("Remove point to " + playername )
     }
     score.innerHTML = new_score
-
     if ( new_score === maxpoints ){
-        // TODO : Winner panel
+        const WinnerPlayer = document.getElementsByClassName('modal-body')
+        WinnerPlayer[0].innerHTML = '<p class="text-center fs-3">'+playername+'</p>'
+        const Winner = new bootstrap.Modal(document.getElementById('winner'), {
+            backdrop: true
+        })
+        Winner.show();
     }
+
+    let close = document.getElementById('modal-reset')
+    close.addEventListener("click", function(){
+        document.querySelectorAll("span#score").forEach(e => { e.innerHTML = "0" })
+    })
+
+
 }
 
-
-
+class BlindTestPlayer extends HTMLElement {
+    constructor (value) {
+        super()
+        this.playerid = value
+        this.innerHTML =  '<div id="'+ this.playerid +'" class="input-group my-1">'+
+            '<button type="button" onclick="remove('+ this.playerid +')" class="btn btn-danger">x</button>' +
+            '<button type="button" onclick="scoring('+ this.playerid +', \'remove\' )" class="btn btn-warning">-</button>' +
+            '<button type="button" onclick="scoring('+ this.playerid +', \'add\' )" class="btn btn-success">+</button>' +
+            '<input type="text" class="form-control" id="playername" placeholder="New player">' +
+            '<span class="input-group-text" id="score">0</span>'+
+            '</div>'
+    }
+}
+customElements.define('blindtest-player', BlindTestPlayer)
